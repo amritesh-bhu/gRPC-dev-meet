22e313ae1a83a91ea1cbdb2479becb7e191f4c07
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-21 00:32:37 +0530

    implemented the services for user regustration!!!

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import grpc from '@grpc/grpc-js';
-import { HTTP_PORT, MONGO_URI } from './.secrets/env.js';
+import protoLoader from '@grpc/proto-loader';
+import { HTTP_PORT, MONGO_URI, PROTO_PATH } from './.secrets/env.js';
 import { dbConnection } from '../config/db-connection/db-conn.js';
+import { rpcAuth } from './rpcRoutes/user-auth.js';
 
 await dbConnection(MONGO_URI)
 
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    oneofs: true,
+    default: true,
+    longs: String,
+    enums: String,
+
+})
+
+const authProto = grpc.loadPackageDefinition(packageDefinition).auth;
 
 const server = new grpc.Server()
+
+server.addService(authProto.AuthService.service, {
+    UserSignUp: rpcAuth.userSignUp
+})
 
 server.bindAsync(`0.0.0.0:${HTTP_PORT}`, grpc.ServerCredentials.createInsecure(), (err, port) => {
     if (err) {
