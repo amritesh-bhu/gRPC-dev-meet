17fead42969e0ae5a02f9d90d08c6a0322395a7b
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-25 00:14:54 +0530

    added the logic at the route layer for user authentication!!!!

--- src/rpcRoutes/user-auth.js
+++ src/rpcRoutes/user-auth.js
@@ -1,5 +1,6 @@
 import grpc from "@grpc/grpc-js"
 import { userDomain } from "../domain/user/index.js"
+import { nanoid } from "nanoid"
 
 export const userSignUp = async (call, callback) => {
     try {
@@ -14,6 +15,26 @@ export const userSignUp = async (call, callback) => {
 
 }
 
+export const userLogin = async (call, callback) => {
+    try {
+        console.log(call)
+        const { emailId, password } = call.request
+        const user = await userDomain.authenticateUser({ emailId, password })
+        if (!user) {
+            throw new Error("Something went wrong!!!")
+        }
+        const sessions = new Map()
+        const sessionId = nanoid(10)
+        sessions.set(sessionId, { emailId, createdAt: Date.now() })
+        callback(null, {
+            sessionId
+        })
+    } catch (err) {
+        callback({ code: grpc.status.NOT_FOUND, details: err.message })
+    }
+}
+
 export const rpcAuth = {
-    userSignUp
+    userSignUp,
+    userLogin
 } 
