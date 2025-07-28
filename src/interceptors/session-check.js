19640d53a83022cedf433b5e64da297f4309ba70
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-29 01:00:09 +0530

    added middleware for the session check of the user on the necessary requests!!

---
+++ src/interceptors/session-check.js
@@ -0,0 +1,27 @@
+import grpc from '@grpc/grpc-js'
+import { sessions } from '../lib/session-in-memory.js'
+
+const userSessionCheck = async (call, callback, next) => {
+    try {
+        const metadata = call.metadata
+        const sessionId = metadata.get('session_id')[0]
+        const session = sessions.get(sessionId)
+        if (!session) {
+            callback({
+                code: grpc.status.UNAUTHENTICATED,
+                details: "Unauthenticated user!!"
+            })
+        }
+        call.sessionId = sessionId
+        next()
+    } catch (error) {
+        callback({
+            code: grpc.status.INTERNAL,
+            details: "Unauthenticated user!!"
+        })
+    }
+}
+
+export const withAuth = (handler) => {
+    return (call, callback) => userSessionCheck(call, callback, () => handler(call, callback))
+}
