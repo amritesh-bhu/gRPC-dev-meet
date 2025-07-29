076797f70abb540332953fdb78f8cbe327f8e55d
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-29 10:23:57 +0530

    code clean up!!

--- src/interceptors/session-check.js
+++ src/interceptors/session-check.js
@@ -6,7 +6,6 @@ const userSessionCheck = async (call, callback, next) => {
         const metadata = call.metadata
         const sessionId = metadata.get('session_id')[0]
         const session = sessions.get(sessionId)
-        console.log(session)
         if (!session) {
             callback({
                 code: grpc.status.UNAUTHENTICATED,
