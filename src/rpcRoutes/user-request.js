d75a69583bc7a8e14a5f8bff6f037ab353dddaa5
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-06 08:52:50 +0530

    added validation things!!

--- src/rpcRoutes/user-request.js
+++ src/rpcRoutes/user-request.js
@@ -1,10 +1,20 @@
 import grpc from "@grpc/grpc-js"
+import { userDomain } from "../domain/user"
 
-const sendRequest = (call, callback) => {
+const sendRequest = async (call, callback) => {
+
     try {
+        const logedInUserId = call.user
 
-        const { status, userId } = call.request
+        const { status, emailId } = call.request
+
+        const isSenderExist = await userDomain.isUserExist({ emailId })
+        if (!isSenderExist) {
+            callback({ code: grpc.status.NOT_FOUND, details: "User with this email does not exist!!!" })
+        }
+
         const allowedStatus = ['Interested', 'Ignored']
+        const isAllowedStatus = allowedStatus.includes(status)
         callback(null, { response: "Request Sent successfully!!" })
     } catch (error) {
         callback({ code: grpc.status.INTERNAL, details: "Something went wrong!!" })
