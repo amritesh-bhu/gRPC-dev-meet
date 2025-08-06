efa97f1199590973ce3f73323c85e1f1bc19d947
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-06 08:51:36 +0530

    checking logedInuser session and attaching it to the request!!

--- src/interceptors/session-check.js
+++ src/interceptors/session-check.js
@@ -1,18 +1,21 @@
 import grpc from '@grpc/grpc-js'
 import { sessions } from '../lib/session-in-memory.js'
+import { userDomain } from '../domain/user/index.js'
 
 const userSessionCheck = async (call, callback, next) => {
     try {
         const metadata = call.metadata
         const sessionId = metadata.get('session_id')[0]
         const session = sessions.get(sessionId)
+        const { emailId } = session
+        const user = await userDomain.viewProfile({ emailId })
         if (!session) {
             callback({
                 code: grpc.status.UNAUTHENTICATED,
                 details: "Unauthenticated user!!"
             })
         }
-        call.session = session
+        call.user = user
         call.sessionId = sessionId
         next()
     } catch (error) {
