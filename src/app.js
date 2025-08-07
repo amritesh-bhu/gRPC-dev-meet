44621f8edb14a94206ba037cb5e8870b2e10c546
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-08 00:59:26 +0530

    modified service name in proto and respective changes in this file!!

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
-import { AUTH_PROTO, HTTP_PORT, MONGO_URI, PROFILE_PROTO } from './.secrets/env.js';
+import { AUTH_PROTO, CONN_PROTO, HTTP_PORT, MONGO_URI, PROFILE_PROTO } from './.secrets/env.js';
 import { dbConnection } from '../config/db-connection/db-conn.js';
 import { rpcAuth } from './rpcRoutes/user-auth.js';
 import { withAuth } from './interceptors/session-check.js';
 import { rpcProfile } from './rpcRoutes/user-profile.js';
+import { rpcConnection } from './rpcRoutes/user-request.js';
 
 await dbConnection(MONGO_URI)
 
-const packageDefinition = protoLoader.loadSync([AUTH_PROTO, PROFILE_PROTO], {
+const packageDefinition = protoLoader.loadSync([AUTH_PROTO, PROFILE_PROTO, CONN_PROTO], {
     keepCase: true,
     oneofs: true,
     default: true,
@@ -29,6 +30,10 @@ server.addService(proto.auth.AuthService.service, {
 server.addService(proto.profile.ProfileService.service, {
     ViewProfile: withAuth(rpcProfile.viewProfile),
     UpdateProfile: withAuth(rpcProfile.updateUserProfile)
+})
+
+server.addService(proto.userConnReq.ConnService.service, {
+    SendRequest: withAuth(rpcConnection.sendConnRequest)
 })
 
 
